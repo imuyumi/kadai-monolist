98f43fdf585cdd9b0c4b37e0b7ee608820c38c43
yumi nishimura <yoshikawa.westup@gmail.com>
2018-11-25 12:56:47 +0000

    want

--- app/Item.php
+++ app/Item.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    //
+    protected $fillable=['code','name','url','image_url'];
+    public function users()
+    //want haveの両方のuserを取得
+    {
+        return $this->belongsToMany(User::class)->withPivot('type')->withTimeStamps();
+    }
+    
+    public function want_users()
+    //wantのみuserを取得する
+    {
+        return $this->users()->where('type','want');
+        //typeがwantのuserを絞り込む
+        
+    }
+    
+      public function have_users()
+    //haveのみuserを取得する
+    {
+        return $this->users()->where('type','have');
+    }
 }
--- app/User.php
+++ app/User.php
@@ -9,21 +9,54 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
+    
+    public function items()
+    {
+        return $this->belongsToMany(Item::class)->withPivot('type')->withTimestamps();
+    }
+    public function want_items()
+    {
+        return $this->items()->where('type','want');
+    }
+    public function want($itemId)
+    {
+        $exist=$this->is_wanting($itemId);
+        if($exist){
+            return false;
+        }else{
+            $this->items()->attach($itemId,['type'=>'want']);
+            return true;
+        }
+    }
+    
+    public function dont_want($itemId)
+    {
+        $exist=$this->is_wanting($itemId);
+        if($exist){
+            \DB::delete("DELETE FROM item_user WHERE user_id=? AND item_id=? AND type='want'",[$this->id,$itemId]);
+        }else{
+            return false;
+        }
+    }
+    public function is_wanting($itemIdOrCode)
+    {
+        if (is_numeric($itemIdOrCode)){
+            $item_id_exists=$this->want_items()->where('item_id',$itemIdOrCode)->exists();
+            return $item_id_exists;
+        }else{
+            $item_code_exists=$this->want_items()->where('code',$itemIdOrCode)->exists();
+            return $item_code_exists;
+        }
+    }
 }
+
+
+
+
